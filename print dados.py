1e53c3eabd25063302643c084620932ccee10637
Janson Panta <128730352+Janson-Panta@users.noreply.github.com>
2023-05-06 21:33:05 -0300

    Add files via upload

---
+++ print dados.py
@@ -0,0 +1,6 @@
+#faça um programa para ler o nome de uma pessoa, sua idade e seu salário e mostre tudo isso na tela.
+
+nome = input("qual o seu nome?")
+idade = input("qual a sua idade?")
+salario = input("qual o seu salário?")
+print(f"{nome} de {idade} anos, ganha {salario} reais por mês")
